1e28ab543141768a8db7aa8db9b0091eb0e733d1
Danil Skachkov <danil.skachkov@gmail.com>
2016-04-01 11:49:22 +0600

    Instruction checking

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ def webhook():
 
 
 def processRequest(req):
-    # if req.get("result").get("action") != "yahooWeatherForecast":
-    #     return {}
+    if req.get("result").get("action") != "yahooWeatherForecast":
+        return {}
     baseurl = "https://query.yahooapis.com/v1/public/yql?"
     yql_query = makeYqlQuery(req)
     if yql_query is None:
