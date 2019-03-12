0a39a49af069f130db5b382135a3f01ddf72a34a
xxyou00 <xingxiangcn@qq.com>
2019-03-12 18:32:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,26 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    if request.method == 'POST':
-        print(request.json)
-        return request.json, 200
+    if request.method == 'GET':
+        verify_token = request.args.get('verify_token')
+        if verify_token == WEBHOOK_VERIFY_TOKEN:
+            authorised_clients[request.remote_addr] = datetime.now()
+            return jsonify({'status':'success'}), 200
+        else:
+            return jsonify({'status':'bad token'}), 401
+
+    elif request.method == 'POST':
+        client = request.remote_addr
+        if client in authorised_clients:
+            if datetime.now() - authorised_clients.get(client) > timedelta(hours=CLIENT_AUTH_TIMEOUT):
+                authorised_clients.pop(client)
+                return jsonify({'status':'authorisation timeout'}), 401
+            else:
+                print(request.json)
+                return jsonify({'status':'success'}), 200
+        else:
+            return jsonify({'status':'not authorised'}), 401
+
     else:
         abort(400)
 
